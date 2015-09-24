72ea79632e295f77c1f67ea08347e18b1be05060
Alejandro Puerta <apuerta@insoftcan.com>
2015-09-24 11:24:54 +0100

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by usuario on 24/9/15.
+//  Copyright © 2015 Insoftcan. All rights reserved.
+//
+
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
