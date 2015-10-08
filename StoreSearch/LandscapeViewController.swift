ed4d134fa827420ec9b5d9155edfaa2ec097c1c2
Alejandro Puerta <apuerta@insoftcan.com>
2015-10-08 14:31:29 +0100

    Pus background to landscape to test Page. 192

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -10,8 +10,38 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     deinit {
         print("deinit \(self)")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
+    }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        
+        pageControl.frame = CGRect(
+            x: 0,
+            y: view.frame.size.height - pageControl.frame.size.height,
+            width: view.frame.size.width,
+            height: pageControl.frame.size.height)
+    }
 }
